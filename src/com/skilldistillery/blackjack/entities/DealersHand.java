cbdc5e3dbb7d9c3f65993ec149b8c8e1cea7a544
mannygutierrez12 <35671250+mannygutierrez12@users.noreply.github.com>
2023-04-07 20:30:40 -0400

    Created multiple classes
    Added 2 more classes, and 1 abstract class.

---
+++ src/com/skilldistillery/blackjack/entities/DealersHand.java
@@ -0,0 +1,33 @@
+package com.skilldistillery.blackjack.entities;
+
+public class DealersHand extends Hand {
+
+	public DealersHand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void hand() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addCard() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getHandValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/com/skilldistillery/blackjack/entities/UsersHand.java
@@ -0,0 +1,39 @@
+package com.skilldistillery.blackjack.entities;
+
+public class UsersHand extends Hand {
+
+	public UsersHand() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void hand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCard() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getHandValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	
+	
+
+}
